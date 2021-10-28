429f5a2c5831dbbb13087bae5a6bf89bb17ab9f0
DiogoReiss <diogommreis1@gmail.com>
2021-10-28 18:34:24 +0100

    fix: making the output more family friendly

--- lib/haly.ex
+++ lib/haly.ex
@@ -1,22 +1,22 @@
 defmodule Haly do
-  def attack(mob_life, damage) do
-    random_damage = damage - :rand.uniform(damage) # 0..damage - damage
-    mob_life - random_damage # life - random_damage
+  def attack(mobLife, damage) do
+    randomDamage = damage - :rand.uniform(damage) # 0..damage - damage
+    mobLife - randomDamage # life - randomDamage
   end
 
-  def is_alive?(rec_damage) do
+  def isAlive(rec_damage) do
     if (rec_damage >= 0) do
-      IO.puts "Ain minha xota"
+      IO.puts "Essa doeu!"
     else
-      IO.puts "Bota pra sentar na PICA"
+      IO.puts "É melhor bater mais forte!"
     end
   end
 
   def main do
-    mob_damage_rec = attack(50, 100)
-    mob_is_alive = is_alive?(mob_damage_rec)
-    IO.puts mob_damage_rec
-    IO.puts mob_is_alive
+    mobDamageRect = attack(50, 100)
+    mobIsAlive = isAlive(mobDamageRect)
+    IO.puts mobDamageRect
+    IO.puts mobIsAlive
     :timer.sleep(1000)
     main()
   end
