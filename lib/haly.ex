ef698c71bf3eacba10944105eb908dd37c606936
DiogoReiss <diogommreis1@gmail.com>
2021-10-29 13:10:11 +0100

    fix: refactoring attack and isAlive functions

--- lib/haly.ex
+++ lib/haly.ex
@@ -1,26 +1,40 @@
 defmodule Haly do
-  def attack(mobLife, damage) do
-    randomDamage = damage - :rand.uniform(damage) # 0..damage - damage
-    mobLife - randomDamage # life - randomDamage
-  end
-
-  def isAlive(rec_damage) do
-    if (rec_damage >= 0) do
-      IO.puts "Essa doeu!"
+  def attack(hero, mob) do
+    if (mob.mob_defense >= hero.attack_prop) do
+      IO.puts "Foi quase :D"
     else
-      IO.puts "É melhor bater mais forte!"
+      damage_taken = mob.mob_life - (hero.attack_prop - mob.mob_defense)
+      mob = %{ mob | mob_life: damage_taken }
+      IO.puts mob.mob_life
+      isAlive(mob.mob_life)
+    end
+
+  end
+  def isAlive(mob_life) do
+    if (mob_life <= 0) do
+      IO.puts "no"
+    else
+      IO.puts "si"
     end
   end
 
   def main do
-    mobDamageRect = attack(50, 100)
-    mobIsAlive = isAlive(mobDamageRect)
-    IO.puts mobDamageRect
-    IO.puts mobIsAlive
+    hero = %{
+      nivel: 1,
+      xp: 0.0,
+      attack_prop: 15
+    }
+
+    mob = %{
+      nivel: 1,
+      xp: 0.0,
+      mob_life: 25,
+      mob_defense: :rand.uniform(20)
+    }
+    attack(hero, mob)
     :timer.sleep(1000)
     main()
   end
 end
-
 
 Haly.main()
